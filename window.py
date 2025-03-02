3ae6f3f6b90ad2522de1ee1a080d81df67bcb038
benjaminafoster <154103844+benjaminafoster@users.noreply.github.com>
2025-03-01 19:14:07 -0500

    added window class and initiated in main

---
+++ window.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.is_running = False
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+        print("program stopped. window closed...")
+
+    def close(self):
+        self.is_running = False
