e93b58db5af74bc7d51327b3dd529cbf7b96289e
benjaminafoster <154103844+benjaminafoster@users.noreply.github.com>
2025-03-01 19:29:35 -0500

    added point and line classes with functions to draw them on the canvas

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from window import Window
+from line import Line
+from point import Point
+
 
 def main():
     win = Window(800, 600)
+    point_1 = Point(25, 25)
+    point_2 = Point(25, 300)
+    line = Line(point_1, point_2)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 main()
