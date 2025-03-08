702bcdb530cd49fc75d75c8b433e3297866c4806
benjaminafoster <154103844+benjaminafoster@users.noreply.github.com>
2025-03-07 23:16:51 -0500

    added function to reset cell visited status after creating the maze

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from maze import Maze
 
 def main():
     win = Window(800, 600)
-    maze_1 = Maze(75, 50, 10, 10, 50, 50, win, 10)
+    maze_1 = Maze(75, 50, 10, 10, 50, 50, win)
     win.wait_for_close()
 
 main()
